3be33fcb31a1866758adb1dd39bf48cb45d31ca4
LandRover <github.com@landroverz.com>
2016-03-08 17:17:51 +0000

    is runnig

--- src/api/routes/is_active.js
+++ src/api/routes/is_active.js
@@ -1,7 +1,11 @@
 'use strict';
 
+import ClashBot from '../../controllers/clashbot';
+
 export default router => {
+  let clasbot = new ClashBot();
+  
   router
     .get('/is_active',
-      async ctx => ctx.body = await Active.find({}))
+      async ctx => ctx.body = await clasbot.isRunning({}))
 };
