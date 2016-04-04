5ae07875925988fad8fc22c79bac0f093e6d4409
LandRover <github.com@landroverz.com>
2016-04-04 14:27:59 +0000

    refactoring

--- app/src/modules/facebook/facebook_controller.js
+++ app/src/modules/facebook/facebook_controller.js
@@ -2,27 +2,31 @@ import angular from 'angular';
 
 export default class FacebookController {
     constructor($scope, $facebook) {
+        this.$scope = $scope;
+        this.$facebook = $facebook;
+        
         $scope.isLoggedIn = false;
         
         $scope.login = () => {
             $facebook.login().then(() => {
-              refresh();
+                this.refresh();
             });
         }
+
+        this.refresh();
+    }
+    
+    
+    refresh() {
+        this.$facebook.api('/me').then(
+            response => {
+                this.$scope.welcomeMsg = 'Welcome ' + response.name;
+                this.$scope.isLoggedIn = true;
+            },
+            err => {
+                this.$scope.welcomeMsg = 'Please log in';
+            });
         
-        function refresh() {
-            $facebook.api('/me').then(
-                response => {
-                    $scope.welcomeMsg = 'Welcome ' + response.name;
-                    $scope.isLoggedIn = true;
-                },
-                err => {
-                    $scope.welcomeMsg = 'Please log in';
-                });
-            
-        }
-        
-        refresh();
     }
 }
 
