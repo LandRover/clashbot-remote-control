e6dc7f68ee91368caf7bb3ffa6216e75abda6fba
LandRover <github.com@landroverz.com>
2016-04-05 07:40:58 +0000

    rewire actions to $scope

--- app/src/modules/app.controllers.js
+++ app/src/modules/app.controllers.js
@@ -2,6 +2,7 @@ let moduleName = 'app.controllers';
 
 import angular from 'angular';
 import UserController from './users/user_controller';
+import MenuController from './ui/menu/menu_controller';
 import ToolbarController from './ui/toolbar/toolbar_controller';
 import SidebarController from './ui/sidebar/sidebar_controller';
 import FacebookController from './facebook/facebook_controller';
@@ -12,6 +13,7 @@ angular.module(moduleName, [])
     .controller('UserController', UserController)
     .controller('ToolbarController', ToolbarController)
     .controller('SidebarController', SidebarController)
+    .controller('MenuController', MenuController)
     ;
 
 export default moduleName;
--- app/src/modules/app.directives.js
+++ app/src/modules/app.directives.js
@@ -4,10 +4,12 @@ import angular from 'angular';
 import directiveFactory from '../utils/directive_factory';
 
 import ToolbarDirective from './ui/toolbar/toolbar_directive';
+import MenuDirective from './ui/menu/menu_directive';
 
 
 angular.module(moduleName, [])
     .directive('clToolbar', directiveFactory(ToolbarDirective))
+    .directive('clMenu', directiveFactory(MenuDirective))
     ;
 
 export default moduleName;
