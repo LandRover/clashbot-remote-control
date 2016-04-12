0fd07c297f0cded6104e30071d4f301f4120b3b4
LandRover <github.com@landroverz.com>
2016-04-12 14:50:04 +0000

    serving index.html via node

--- server/src/defaults/index.js
+++ server/src/defaults/index.js
@@ -1,9 +1,14 @@
 'use strict';
 
+const fs = require('fs'),
+      indexPath = __dirname + '/../../../app/index.html';
+
 export default function defaults(ctx, next) {
+    let index = fs.readFileSync(indexPath);
+
     return next().then(() => {
         if ('/' === ctx.path) {
-            ctx.body = 'Hello';
+            ctx.body = index.toString();
         }
     });
 }
