e07ede530bdabb68b248a390de74076092af7af2
LandRover <github.com@landroverz.com>
2016-04-15 06:36:23 +0000

    status checking added

--- app/src/modules/dashboard/dashboard_controller.js
+++ app/src/modules/dashboard/dashboard_controller.js
@@ -1,10 +1,33 @@
 import angular from 'angular';
 
+const text = {
+    'true': 'Im is running.. your donations are on their way',
+    'false': 'Im is NOT running.. you may start me.'
+};
+
 export default class DashboardController {
-    constructor($scope, FacebookService) {
+    constructor($scope, FacebookService, ClashbotService) {
         this.$scope = $scope;
         this.FacebookService = FacebookService;
+        this.ClashbotService = ClashbotService;
+        
+        
+
+        this.checkStatus();
+    }
+    
+    
+    checkStatus() {
+        this.$scope.status = 'checking status... please wait.'
+        this.$scope.loadingProgress = true;
+        
+        this.ClashbotService.status().then(response => {
+            this.$scope.loadingProgress = false;
+            
+            this.$scope.status = text[response];
+            
+        });
     }
 }
 
-DashboardController.$inject = ['$scope', 'FacebookService'];
+DashboardController.$inject = ['$scope', 'FacebookService', 'ClashbotService'];
