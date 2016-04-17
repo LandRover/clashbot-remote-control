ab58c9f00db895cac650e148fbcafe37ab1185a5
LandRover <github.com@landroverz.com>
2016-04-17 06:25:23 +0000

    st

--- app/src/modules/proxy/proxy_controller.js
+++ app/src/modules/proxy/proxy_controller.js
@@ -75,7 +75,7 @@ export default class ProxyController {
         
         this.ClashbotService.log().then(response => {
             this.$scope.loadingProgress = false;
-            this.$scope.remoteAppStatus = angular.element('<pre>'+ response.data +'</pre>');
+            this.$scope.remoteAppStatus = this.$sce.trustAsHtml('<strong>'+ response.data +'</strong>');
         });
     }
     
